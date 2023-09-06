4fde4c4c0c18a7b7bc10bb87f50eb35bbff7a29b
frendsick <teemu@paddy.fi>
2023-09-06 23:07:22 +0300

    Implement Functor interface for Parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,3 +3,8 @@ module Parser where
 newtype Parser a = Parser
   { runParser :: String -> Maybe (a, String)
   }
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input -> do
+    (result, input') <- p input
+    Just (f result, input')
