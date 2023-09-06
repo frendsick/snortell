feb5427a132a74786815c1607855d4c16e81f51c
frendsick <teemu@paddy.fi>
2023-09-06 23:19:04 +0300

    Implement Applicative interface for Parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -8,3 +8,11 @@ instance Functor Parser where
   fmap f (Parser p) = Parser $ \input -> do
     (result, input') <- p input
     Just (f result, input')
+
+instance Applicative Parser where
+  pure value = Parser $ \input -> Just (value, input)
+
+  (Parser p1) <*> (Parser p2) = Parser $ \input -> do
+    (f, input') <- p1 input
+    (result, input'') <- p2 input'
+    Just (f result, input'')
