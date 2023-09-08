68ed5ee6f9f37f612cebc4209315d07ff54c5dad
frendsick <teemu@paddy.fi>
2023-09-09 00:38:55 +0300

    Read multiple rules from a file
    Only one rule per line is supported.
    Multiline rules exist with backslashes at the end of lines.

--- src/Main.hs
+++ src/Main.hs
@@ -8,9 +8,18 @@ main = do
     [] -> putStrLn "Usage: ./snortell <file_path> [file_path ...]"
     filePaths -> mapM_ processSnortFile filePaths
 
+-- Read Snort rules from the specified file
+-- Only one rule per line is supported
 processSnortFile :: FilePath -> IO ()
 processSnortFile filePath = do
-  rule <- readFile filePath
+  -- Read the content of the file and split it into lines
+  snortRules <- lines <$> readFile filePath
+  -- Process each rule separately
+  mapM_ (parseSnortRule filePath) snortRules
+
+-- Process an individual Snort rule, including the filePath in the messages
+parseSnortRule :: FilePath -> String -> IO ()
+parseSnortRule filePath rule = do
   case parseSnort rule of
-    Left err -> putStrLn $ "Error parsing Snort rule in file '" ++ filePath ++ "': " ++ err
-    Right parsedRule -> putStrLn $ "Parsed Snort rule in file '" ++ filePath ++ "': " ++ show parsedRule
+    Left err -> putStrLn $ "Error parsing Snort rule in " ++ filePath ++ ": " ++ err
+    Right parsedRule -> putStrLn $ "Parsed Snort rule in " ++ filePath ++ ": " ++ show parsedRule
